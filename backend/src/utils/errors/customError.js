985a5e210e8b402920ded5ba5fc25ef541bb28d1
woscoff <73312435+woscoff@users.noreply.github.com>
2023-07-24 17:31:13 -0300

    final commit

---
+++ backend/src/utils/errors/customError.js
@@ -0,0 +1,14 @@
+export default class CustomError extends Error {
+    constructor({ name = `Error`, cause, message, code = 4 }) {
+        super(message);
+        this.name = name;
+        this.cause = cause;
+        this.code = code;
+    }
+
+    static createError({ name = `Error`, cause, message, code = 4 }) {
+        const error = new CustomError({ name, cause, message, code });
+        console.log(`${error.name}: ${error.cause}`)
+        throw error;
+    }
+}
